05dd3f1aee3fb02a6fec9ed5cd9e6135233f3e2c
Shahar Yifrah <shaharyi88@gmail.com>
2020-07-30 10:47:48 +0300

    minor fixes

--- flaskr/ultimate_tictactoe.py
+++ flaskr/ultimate_tictactoe.py
@@ -9,7 +9,7 @@ from ultimate_tictactoe.state import UltimateTicTacToeMove, UltimateTicTacToeGam
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 
 N = 3
-NUM_ROLLOUTS = 100
+NUM_ROLLOUTS = 10
 
 bp = Blueprint('ultimate_tictactoe', __name__, url_prefix='/ultimate_tictactoe')
 state = None
@@ -27,12 +27,14 @@ def pos(i):
 def game_restart():
     global state, N
     board = np.zeros((N, N, N, N), int)
-    state = UltimateTicTacToeGameState(board=board, next_to_move=-1)
+    state = UltimateTicTacToeGameState(board=board, next_to_move=1)
     form = UltimateTictactoeForm()
     legal_moves = state.get_legal_actions(as_coords=True)
     mainboard = state.main_board()
+    game_over, desig_board = None, None
     return render_template('ultimate_tictactoe.html', form=form, N=N,
-                           game_over=False, board=state.board, desig_board=None,
+                           game_over=game_over, board=state.board,
+                           desig_board=desig_board, last_move=state.last_move,
                            legal_moves=legal_moves, mainboard=mainboard)
 
 
@@ -41,10 +43,11 @@ def game():
     global state, N
     form = UltimateTictactoeForm()
     if form.validate_on_submit():
-        if not state.is_game_over() and state.next_to_move == -1:
+        print(state.main_board())
+        if not state.is_game_over() and state.next_to_move == 1:
             m = request.form['pressed']
             m = int(m)
-            action = UltimateTicTacToeMove(pos(m), -1)
+            action = UltimateTicTacToeMove(pos(m), 1)
             state = state.move(action)
             if not state.is_game_over():
                 root = TwoPlayersGameMonteCarloTreeSearchNode(state=state)
@@ -55,13 +58,15 @@ def game():
 
     game_over = state.is_game_over()
     if game_over:
-        flash(('X wins!', 'Draw!', 'O wins!')[state.game_result + 1])
+        flash(('O wins!', 'Draw!', 'X wins!')[state.game_result + 1])
 
     legal_moves = state.get_legal_actions(as_coords=True)
     mainboard = state.main_board()
     desig_board = state.last_move and state.last_move.pos[2:] or None
     if desig_board and mainboard[desig_board] != 0:
         desig_board = None
+    print(mainboard)
     return render_template('ultimate_tictactoe.html', form=form, N=N,
-                           game_over=game_over, board=state.board, desig_board=desig_board,
+                           game_over=game_over, board=state.board,
+                           desig_board=desig_board, last_move=state.last_move,
                            legal_moves=legal_moves, mainboard=mainboard)
